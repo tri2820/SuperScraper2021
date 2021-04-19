2d897bbfde99c4fcf7e49463fed07c27579b42cf
Hamish Eefting <heefting@deakin.edu.au>
2021-04-20 04:05:40 +1000

    -

--- Scraper/scraper_run.py
+++ Scraper/scraper_run.py
@@ -54,9 +54,9 @@ aware_fund_data = db_connection.retrieve_fund_data("aware")
 telstra_fund_data = db_connection.retrieve_fund_data("telstra")
 db_connection.close_connection()
 
-#process.crawl('Aware', fund_data = aware_fund_data)
+process.crawl('Aware', fund_data = aware_fund_data)
 
-#process.crawl('Telstra', fund_data = telstra_fund_data)
+process.crawl('Telstra', fund_data = telstra_fund_data)
 
 process.crawl('Hesta', fund_data = hesta_fund_data)
 
