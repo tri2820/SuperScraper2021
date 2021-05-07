3af82c948848cbe57aac2d32f1749b66d2a1f4ee
DESKTOP-T8K0ROT\Thakoor <mthakoor@deakin.edu.au>
2021-05-08 02:13:04 +0400

    Added documentation for telstra

--- Scraper/spiders/super_telstra.py
+++ Scraper/spiders/super_telstra.py
@@ -14,6 +14,29 @@ from Scraper.spiders.super_base import BaseSpider
 
 #https://www.telstrasuper.com.au/api/Investment/DownloadMonthlyInvestmentPerformance?SelectedOptions=GROW%2CBAL%2CDEFG%2CCONS%2CINTL%2CAUST%2CPROP%2CFINT%2CCASH%2CINC&Category=AC&DataType=performancepercentage&DateRange=12m&ShowAll=1
 
+'''
+TELSTRASPIDER
+
+Class Variables:
+- start_urls[list]: a list of urls that the spider will begin to crawl from when no particular URLs are specified
+
+- name: a string which defines the name for the current spider
+
+ - crawl_selections[list][tuples]: (parse_select, url)
+    - url[string]: url parsed to scrapy spider, url to goto and scrap.
+    - parse_select[string]: a string variable that matches the name of the function to be run (eg: 'parse_monthly', 'parse_hist')
+
+- fund_data[object(dict-like)]: metadata given for spider to run (typically pulled from the mongodb database).
+
+ --- parse_monthly ---
+The parse method is responsible for processing the response and returning the scraped data and/or more URLs to follow.
+Returns an iterable of requests or items[object](dict-like)].
+
+Parameters:
+ - response: It is an object in the form of an HTTP response that is fed to the spider for processing
+
+'''
+
 class TelstraSpider(scrapy.Spider):
     name = "Telstra"
 
