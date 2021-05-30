a953b8fb2b9bcd05a3697d6f35223f83d3c1b18f
Zhafran <mzathaillah@deakin.edu.au>
2021-05-30 14:44:55 +1000

    Adding comments for Main.js, Navigation bar, and charts on html files

--- UI/main.js
+++ UI/main.js
@@ -1,6 +1,7 @@
 const { app, BrowserWindow } = require('electron')
 const path = require('path')
 
+// Creating Desktop Window and loading index.html 
 function createWindow () {
   const win = new BrowserWindow({
     width: 800,
@@ -13,6 +14,7 @@ function createWindow () {
   win.loadFile('index.html')
 }
 
+// Enabling browsing via desktop window 
 app.whenReady().then(() => {
   createWindow()
 
@@ -23,6 +25,7 @@ app.whenReady().then(() => {
   })
 })
 
+// Intergrating python script with electron JS 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit()
