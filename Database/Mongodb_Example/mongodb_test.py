0f889d622ebe5f1da194f19b47e3a2f12c4e44fc
Hamish Eefting <heefting@deakin.edu.au>
2021-06-02 04:00:54 +1000

    - Updated readme.md files added more installation stuff - Edited some other stuff

--- Database/Mongodb_Example/mongodb_test.py
+++ Database/Mongodb_Example/mongodb_test.py
@@ -7,6 +7,8 @@ MONGODB_DB = "SuperScrapper"
 # These are collection names(currently in database)
 MONGODB_COLLECTIONS = ["funds","offerings"]
 
+''' --- Needs possible updating --- '''
+
 '''
 
 Before using this scipt:
