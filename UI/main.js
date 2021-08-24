4e35b914f2c626fc5bcf61029c5abdc992e7ef45
Zhafran <mzathaillah@deakin.edu.au>
2021-08-24 16:26:32 +1000

    Adding CSV reader and assign to key value pair

--- UI/main.js
+++ UI/main.js
@@ -1,5 +1,19 @@
 const { app, BrowserWindow } = require('electron')
 const path = require('path')
+var fs = require('fs');
+
+var dropdowns = {};
+//dropdown["telsta"] = "AASAS"
+//dropdown["telsta"] = "AASAS"
+//dropdown["telsta"] = "AASAS"
+
+// ^^^
+
+// <li>Telstra</li>
+// <ul>dsfds</ul>
+
+
+//create function thenj call in index.html
 
 // Creating Desktop Window and loading index.html 
 function createWindow () {
@@ -12,6 +26,33 @@ function createWindow () {
   })
 
   win.loadFile('index.html')
+
+  win.webContents.once('dom-ready', () => {
+    var fileData = path.join(__dirname, 'performance.csv');
+    fs.readFile(fileData, 'utf8', function (err, filedata) {
+      if (err) return console.log(err);
+      var data = filedata.split("\n");
+      data.splice(0, 1);
+      for(var b = 0; b < data.length; b++) {
+          var colData = data[b].split(",");
+          var parts = colData[0].split(" ");
+          if (parts[0] === "") {
+            continue;
+          }
+          if (!(parts[0] in dropdowns)) {
+              dropdowns[parts[0]] = [];
+          }
+          dropdowns[parts[0]].push(colData[0].substring(parts[0].length + 1));
+      }
+      // var iframe = document.createElement('iframe');
+      // iframe.addAtributes("class", ".drpbns");
+      // iframe.id = 'iframe';
+      // iframe.style.display = 'none';
+      // document.body.appendChild(iframe);
+      // iframe.src = 'testing1.txt';
+    });
+    
+  });
 }
 // Enabling browsing via desktop window 
 app.whenReady().then(() => {
@@ -39,3 +80,4 @@ app.on('window-all-closed', () => {
     app.quit()
   }
 })
+
