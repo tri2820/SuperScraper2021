552403cdabdaa5a2d99df5b00d9490490a66bfbf
Hamish Eefting <heefting@deakin.edu.au>
2021-09-06 11:50:30 +1000

    - Finished subtable testing - Create class to handle and create subtables and table and subtable formating/creating/extraction

--- new_table_testing.py
+++ new_table_testing.py
@@ -22,6 +22,8 @@ from scipy import stats as sci_stats
 
 import math
 
+import pandas as pd
+
 
 
 def font_size_filter(in_object):
@@ -66,6 +68,400 @@ search_texts = ['asset class']
 r = requests.get(url)
 f = io.BytesIO(r.content)
 
+
+class Table:
+
+    def __init__(self):
+        """
+        Variables:
+        """
+
+        self.page_ = None
+
+        self.word_lines = {}
+        self.table_collections = []
+
+        return
+    
+
+    def extract_words(self, page_):
+        if not page_:
+            page_ = self.page_
+        # Extract words
+        words = page_.extract_words(x_tolerance=1, y_tolerance=1, keep_blank_chars=True)
+
+        # Create lines from words based on vertical positioning [1px dif]
+        for word_obj in words:
+            bottom_pos = int(word_obj['bottom'])
+            pos_range = [bottom_pos, bottom_pos - 1, bottom_pos + 1]
+            
+            current_word_line = None
+            for pos in pos_range:
+                if pos in self.word_lines:
+                    current_word_line = self.word_lines[pos]
+                    break
+            if not current_word_line:
+                self.word_lines[bottom_pos] = {
+                    'line_rect': {},
+                    'rects': [],
+                    'text': '',
+                    'search_found': None,
+                    'word_groups': [],
+                }
+                current_word_line = self.word_lines[bottom_pos]
+            # --
+            current_word_line['rects'].append(word_obj)
+        # --
+        word_line_rects = []
+        for pos in self.word_lines:
+            
+            word_rects = self.word_lines[pos]['rects']
+            word_rects = sorted(word_rects, key=lambda word: word["x0"], reverse=False)
+            
+            line_text = ''
+            for word_rect in word_rects:
+                line_text += word_rect['text'] + ' '
+            
+            self.word_lines[pos]['text'] = line_text
+            self.word_lines[pos]['rects'] = word_rects
+            
+            # Rects stats
+            for rect in word_rects:
+                x0 = int(rect['x0'])
+                x1 = int(rect['x1'])
+                rect['x_mid'] = (x0 + x1) / 2
+            
+            
+            #word_line = self.word_lines[pos]
+            
+            line_rect = {
+                'x0': word_rects[0]['x0'],
+                'x1': word_rects[-1]['x1'],
+
+                'x_mid': (word_rects[0]['x0'] + word_rects[-1]['x1']) / 2,
+                
+                'width': word_rects[0]['x0'] - word_rects[-1]['x1'],
+                'height': word_rects[0]['top'] - word_rects[-1]['bottom'],
+                
+                'top': word_rects[0]['top'],
+                'bottom': word_rects[0]['bottom']
+            }
+            self.word_lines[pos]['line_rect'] = line_rect
+            word_line_rects.append(line_rect)
+        # --
+    
+
+    def search_for_subtables(self, search_regexs):
+        # Search for potential subtables with regex
+        for pos in self.word_lines:
+            word_line = self.word_lines[pos]
+            found = False
+            for search_regex in search_regexs:
+                #found_idx = word_line['text'].lower().find(search_word)
+                #if found_idx != -1:
+                found_idx = re.search(search_regex['search'], word_line['text'].lower())#.find(search_word)
+                if found_idx != None:
+                    word_line['search_found'] = search_regex['label']
+                    found = True
+                    break
+            # --
+            if not found:
+                continue
+            # --
+            print('\n -- FOUND -- \n')
+        # --
+        return
+    
+
+    def construct_subtables(self, line_leniency=2):# TODO: Add args and have args for discard ect...
+
+
+        line_indecies = list(self.word_lines.keys())
+        line_indecies = sorted(line_indecies, reverse=False)
+
+        table_line_rects = []
+        self.table_collections = []
+        
+        for pos in self.word_lines:
+            word_line = self.word_lines[pos]
+            if not word_line['search_found']:
+                continue
+            print(f'\n - Constructing table for {word_line["search_found"]}')
+            
+            table_lines = []
+            line_idx = line_indecies.index(pos)
+            table_lines.append(self.word_lines[pos])
+            #table_line_rects.append(self.word_lines[pos]['line_rect'])
+            
+            n = len(line_indecies) - 1
+            count = 1
+            last_idx = line_idx + 1
+            last_line = self.word_lines[pos]
+            extra_lines = 0
+            last_line_failed = False
+            last_line_failed_idx = 0
+            while last_idx < n:
+                count += 1
+                line_pos = line_indecies[last_idx]
+                next_line = self.word_lines[line_pos]
+                
+                # If last line inside next line
+                if next_line['line_rect']['top'] < last_line['line_rect']['bottom']:
+                    #print(last_line['line_rect']['top'], next_line['line_rect']['bottom'])
+                    table_lines.append(next_line)
+                    #table_line_rects.append(next_line['line_rect'])
+                    
+                    last_line = next_line
+                    last_idx += 1
+                    
+                    continue
+                # --
+                
+                line_text = next_line['text']
+                
+                patterns = []
+                pattern = '[\d]+'
+                
+                found_re = re.search(pattern,line_text)
+                if not found_re:
+                    extra_lines += 1
+                    last_line_failed = True
+                    last_line_failed_idx = max(1, len(table_lines) - 1)
+                    if extra_lines > line_leniency:
+                        break
+                else:
+                    last_line_failed = False
+                # --
+                
+                table_lines.append(next_line)
+                #table_line_rects.append(next_line['line_rect'])
+                
+                last_line = next_line
+                last_idx += 1
+            # --
+            
+            # If failed cut back to prvious
+            if last_line_failed:
+                table_lines = table_lines[:last_line_failed_idx - 1]
+            
+            table_collection_obj = {
+                'table_lines': table_lines,
+                'table_columns': []
+            }
+
+            self.table_collections.append(table_collection_obj)#table_lines
+            
+            
+            for x in table_lines:
+                table_line_rects.append(x['line_rect'])
+            # --
+            
+            # --
+        # --
+        return
+    
+
+    def format_subtables(self):
+        for table_collection in self.table_collections:
+            print('\n - New Table/collection - \n')
+
+            """
+            -- Create and seperate word group columns --
+            """
+
+            table_line_groups = []
+            for table_line in table_collection['table_lines']:
+                rects = table_line['rects']
+
+                rects_txt = [x['text'] for x in rects]
+                rect_sides_left = [x['x0'] for x in rects]
+                rect_sides_right = [x['x1'] for x in rects]
+                rect_lengths = [x['x1'] - x['x0'] for x in rects]
+
+                rect_dists = []
+                before_rect = rects[0]
+                for idx in range(1, len(rects)):
+                    cur_rect = rects[idx]
+                    cur_rect['dist'] = math.inf
+
+                    distance = abs(cur_rect['x0'] - before_rect['x1'])
+                    before_rect['dist'] = distance
+
+                    rect_dists.append(distance)
+                    before_rect = cur_rect
+
+                #print(rect_dists)
+
+                if len(rect_dists) == 0:
+                    rect_dists = [0]
+
+                average_dist = sum(rect_dists) / max(1,len(rect_dists))
+                dist_modes = sci_stats.mode(rect_dists)
+                dist_mode = list(dist_modes)[0]
+                mode_threshold = int(math.ceil(dist_mode) * 1.25)
+                #print(f'Mode thresh: {mode_threshold} - Avg dist: {average_dist}')
+
+                word_groups = []#rect_groups
+                cur_group = []
+                cur_text = ''
+                for idx in range(len(rects)):
+                    cur_rect = rects[idx]
+                    #print(f"- {cur_rect['text']} - {cur_rect['dist']} ")
+                    half_avg = False
+                    small_value = False
+
+                    if 'dist' in cur_rect:
+                        half_avg = cur_rect['dist'] < (average_dist / 2) + min(2, mode_threshold)
+                        small_value = cur_rect['dist'] < min(6, mode_threshold)
+                    if half_avg and small_value:
+                        cur_text += cur_rect['text'] + ' '
+                        cur_group.append(cur_rect)
+                    else:
+                        #if len(cur_group) == 0:
+                        #    continue
+                        cur_text += cur_rect['text'] + ' '
+                        cur_group.append(cur_rect)
+
+                        group_rect = {
+                            'x0': cur_group[0]['x0'],
+                            'x1': cur_group[-1]['x1'],
+                            
+                            'width': cur_group[0]['x0'] - cur_group[-1]['x1'],
+                            'height': cur_group[0]['top'] - cur_group[-1]['bottom'],
+
+                            'x_mid': (cur_group[0]['x0'] + cur_group[-1]['x1']) / 2,
+                            
+                            'top': cur_group[0]['top'],
+                            'bottom': cur_group[0]['bottom']
+                        }
+
+                        group_obj = {
+                            'rects': cur_group,
+                            'text': cur_text,
+                            'group_rect': group_rect,
+                            'range_idx': None,
+                        }
+                        word_groups.append(group_obj)
+                        cur_group = []
+                        cur_text = ''
+                # --
+
+
+                table_line['word_groups'] = word_groups
+                table_line_groups.append(word_groups)
+
+                
+
+                print('\n')
+                [print(x['text']) for x in word_groups]
+                #im.draw_rects([x['group_rect'] for x in word_groups], stroke=(255, 66, 144), fill=(255, 0, 46, 30))
+            # --
+
+
+            """
+            -- Create and seperate word group columns --
+            """
+            x_range_list = []
+            # Each lines groups
+            #for word_groups in table_line_groups:
+            for table_line in table_collection['table_lines']:
+                word_groups = table_line['word_groups']
+                # Groups on line
+                for word_group in word_groups:
+                    group_rect = word_group['group_rect']
+                    new_x_range = (group_rect['x0'], group_rect['x1'], group_rect['top'], group_rect['bottom'])
+                    exists = False
+                    for idx, x_range in enumerate(x_range_list):
+                        x_range_mid = (x_range[0] + x_range[1]) / 2
+                        if (group_rect['x_mid'] >= x_range[0] and group_rect['x_mid'] <= x_range[1]) or (x_range_mid >= new_x_range[0] and x_range_mid <= new_x_range[1]):
+                            new_x_range = (min(new_x_range[0], x_range[0]), max(new_x_range[1], x_range[1]), min(new_x_range[2], x_range[2]), max(new_x_range[3], x_range[3]))
+                            x_range_list[idx] = new_x_range
+                            word_group['range_idx'] = idx
+                            exists = True
+                            break
+                    
+                    if not exists:
+                        word_group['range_idx'] = len(x_range_list)
+                        x_range_list.append(new_x_range)
+                    # --
+            # --
+            table_collection['column_ranges'] = x_range_list
+
+            table_columns_rects = []
+            for x_range in x_range_list:
+                rect_ = {'x0': x_range[0], 'x1': x_range[1], 'top': x_range[2], 'bottom': x_range[3]}
+                table_columns_rects.append(rect_)
+            #im.draw_rects(table_columns_rects, stroke=(255, 66, 144), fill=(255, 0, 46, 30))
+            # --
+        return
+    
+    def create_subtable_dataframes(self):
+        for table_collection in self.table_collections:#columns
+            column_ranges = table_collection['column_ranges']
+            df_rows = [[None] * len(column_ranges) for x in table_collection['table_lines']]
+            df_rows = np.array(df_rows)
+            for line_idx, table_line in enumerate(table_collection['table_lines']):
+                word_groups = table_line['word_groups']
+                # Groups on line
+                for word_group in word_groups:
+                    range_idx = word_group['range_idx']
+                    df_rows[line_idx, range_idx] = word_group['text']
+            collection_df = pd.DataFrame(df_rows)
+            print(collection_df)
+        return
+
+            
+
+'''
+x_range_list = []
+# Each lines groups
+for word_groups in table_line_groups:
+    # Groups on line
+    for word_group in word_groups:
+        group_rect = word_group['group_rect']
+        new_x_range = (group_rect['x0'], group_rect['x1'], group_rect['top'], group_rect['bottom'])
+        exists = False
+        for idx, x_range in enumerate(x_range_list):
+            x_range_mid = (x_range[0] + x_range[1]) / 2
+            if (group_rect['x_mid'] >= x_range[0] and group_rect['x_mid'] <= x_range[1]) or (x_range_mid >= new_x_range[0] and x_range_mid <= new_x_range[1]):
+                new_x_range = (min(new_x_range[0], x_range[0]), max(new_x_range[1], x_range[1]), min(new_x_range[2], x_range[2]), max(new_x_range[3], x_range[3]))
+                x_range_list[idx] = new_x_range
+                word_group['range_idx'] = idx
+                exists = True
+                break
+        
+        if not exists:
+            word_group['range_idx'] = len(x_range_list)
+            x_range_list.append(new_x_range)
+        # --
+# --
+
+'''
+
+
+
+with pdfplumber.open(f) as pdf:
+    page = pdf.pages[pg_no]
+    
+    page = page.filter(font_size_filter)
+
+    new_table = Table()
+
+    new_table.extract_words(page)
+    search_regex = [
+        {
+            'search': 'asset class',
+            'label': 'asset_class',
+        }
+    ]
+    new_table.search_for_subtables(search_regex)
+    new_table.construct_subtables(2)
+    new_table.format_subtables()
+    new_table.create_subtable_dataframes()
+
+
+'''
+# --
 
 with pdfplumber.open(f) as pdf:
     page = pdf.pages[pg_no]
@@ -133,8 +529,6 @@ with pdfplumber.open(f) as pdf:
         line_rect = {
             'x0': word_rects[0]['x0'],
             'x1': word_rects[-1]['x1'],
-            #'y0': word_rects[0]['y0'],
-            #'y1': word_rects[-1]['y1'],
 
             'x_mid': (word_rects[0]['x0'] + word_rects[-1]['x1']) / 2,
             
@@ -390,14 +784,14 @@ with pdfplumber.open(f) as pdf:
         for x_range in x_range_list:
             rect_ = {'x0': x_range[0], 'x1': x_range[1], 'top': x_range[2], 'bottom': x_range[3]}
             col_draw_rects_.append(rect_)
-        im.draw_rects(col_draw_rects_, stroke=(255, 66, 144), fill=(255, 0, 46, 30))
+        im.draw_rects(col_draw_rects_, stroke=(255, 66, 144), fill=(255, 0, 46, 30))'''
 
 
         # TODO: Make arguments things for each line to be run thing
 
 
 
-        '''
+'''
         kde = KernelDensity(kernel='gaussian', bandwidth=4)
 
         print(mids)
@@ -513,13 +907,13 @@ with pdfplumber.open(f) as pdf:
 
     #for rect in table_line_rects:
     
-    im.draw_rects(table_line_rects, stroke=(35, 61, 255), fill=(35, 61, 255, 30))
+    #im.draw_rects(table_line_rects, stroke=(35, 61, 255), fill=(35, 61, 255, 30))
     #im.draw_rects(words)
     #im.draw_rects(word_line_rects_not_table, stroke=(102, 255, 68), fill=(102, 255, 68, 30))
     
     
     # Save
-    im.save('test_img.png', format="PNG")
+    #im.save('test_img.png', format="PNG")
 # --
 
 
